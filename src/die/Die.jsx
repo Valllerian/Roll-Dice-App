39d9773a3b52997d1b980ec2010ee44d678a977d
Valerii Bihun <valeriibihun.co@gmail.com>
2022-03-22 20:38:05 -0600

    file tree and imports added

---
+++ src/die/Die.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './die.css'
+
+const Die = () => {
+  return (
+    <div>Die</div>
+  )
+}
+
+export default Die
---
+++ src/rollDice/RollDice.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Die from '../die/Die'
+import './rollDice.css'
+
+const RollDice = () => {
+  return (
+    <div>RollDice</div>
+  )
+}
+
+export default RollDice
