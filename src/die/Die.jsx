32f96c96cc68b42cae01fa6cdad0a4b2d5cc38d0
Valerii Bihun <valeriibihun.co@gmail.com>
2022-03-22 22:09:08 -0600

    Die color changed

--- src/die/Die.jsx
+++ src/die/Die.jsx
@@ -5,8 +5,8 @@ const Die = (props) => {
   let number = props.number;
   return (
     <div className={props.shaking ? 'shaking' : null}>
-      <div className="mx-6 my-6">
-        <i className={`fas fa-dice-${number} fa-10x`}></i>
+      <div className="mx-6 my-6 text-midnight">
+        <i className={`fas fa-dice-${number} fa-10x `}></i>
       </div>
     </div>
   );
--- src/rollDice/RollDice.jsx
+++ src/rollDice/RollDice.jsx
@@ -5,24 +5,21 @@ import "./rollDice.css";
 const RollDice = () => {
   const [numberOne, setNumberOne] = useState("one");
   const [numberTwo, setNumberTwo] = useState("one");
-  const [shaking, setShaking] = useState(false)
+  const [shaking, setShaking] = useState(false);
 
   function handleClick(e) {
     e.preventDefault();
-  
-    setShaking(true)
-    console.log(shaking)
+    setShaking(true);
     const numbers = ["one", "two", "three", "four", "five", "six"];
-    setTimeout(function(){
-        let numberOne = setNumberOne(
-            numbers[Math.floor(Math.random() * numbers.length)]
-          );
-          let numberTwo = setNumberTwo(
-            numbers[Math.floor(Math.random() * numbers.length)]
-          );
-          setShaking(false)
-          
-    }, 1000)
+    setTimeout(function () {
+      let numberOne = setNumberOne(
+        numbers[Math.floor(Math.random() * numbers.length)]
+      );
+      let numberTwo = setNumberTwo(
+        numbers[Math.floor(Math.random() * numbers.length)]
+      );
+      setShaking(false);
+    }, 1000);
   }
   return (
     <div className="grid place-items-center h-screen">
@@ -34,7 +31,13 @@ const RollDice = () => {
           <Die shaking={shaking} number={numberTwo} />
         </div>
       </div>
-      <button onClick={handleClick} className="ease inline-block cursor-pointer rounded-full bg-pink-600 px-8 py-3 text-7xl text-white transition duration-500 hover:bg-indigo-900"> Roll Dice!</button>
+      <button
+        onClick={handleClick}
+        className="ease inline-block cursor-pointer rounded-full bg-pink-600 px-8 py-3 text-7xl text-white transition duration-500 hover:bg-indigo-900"
+      >
+        {" "}
+        Roll Dice!
+      </button>
     </div>
   );
 };
